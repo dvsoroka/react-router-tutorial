f21a3eb9e019e5c5e583ad831c56515cc7115d12
dvsoroka <dvsoroka@gmail.com>
2023-12-21 12:53:40 +0200

    react-router-tutorial Lesson2

--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { BrowserRouter as Router } from "react-router-dom"            // as Router, or Routes, or Route 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+  
   return (
-    <h1>Hello, React Router!</h1>
+    <BrowserRouter>
+      <nav>
+        <Link className='someClass' to="/">Home</Link>
+        <Link className='someClass' to="about">About</Link>
+
+      </nav>
+      <Routes>
+        <Route 
+          path="/" 
+          element={
+            <Home 
+              count={count} 
+              setCount={setCount}
+              />
+          } />   
+        <Route path="/about" element={<About count={count}/>} /> 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-//example.com/about
-//example.com/contact
-//example.com/blog/blog-article-1
-//example.com/blog/1234
 
+function Home(props) {
+//  console.log(props)
+  let count = props.count
+  return (
+    <div>
+        <h1>Hello, React Router!</h1>
+        
+        <button onClick={() => props.setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+    </div>
+  )
+}
 
-
-function About() {
+function About(props) {
     return (
+      <>
         <h1>About page goes here! 🎉</h1>
+        <button>count is {props.count}</button>
+      </>
     )
 }
-/**
- * Challenge:
- * 1. Create an "About" component (just render another h1 that says
- *    "About page" or something more interesting of your choosing).
- * 2. Create a new Route to render the About component when the path
- *    is /about
- */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />   
-      <Route path="/about" element={<About />} /> 
-    </Routes>
-  </BrowserRouter>
+  <App />
     
 );
 
